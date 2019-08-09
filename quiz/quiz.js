17ac993fda6180f2e0a0079b2bed4a476ed5e75b
Arshak94 <arshak.tovmasyan.94@gmail.com>
2019-08-09 18:25:00 +0400

    09.08.2019 from IUNet form MacbookPro

--- quiz/quiz.js
+++ quiz/quiz.js
@@ -1,14 +1,16 @@
-var date = new Date();
+let date = new Date();
 
 var username;
-var quiz;
+let quiz;
+let timer = 0;
+let points = 0;
 
 socket.on('logedin', function (data) {
     if (am_i(data[0], code)) {
         username = data[1].username;
         document.getElementById('loading').style.display = 'block';
         document.getElementById('loading' + (Math.floor(Math.random() * 5) + 1)).style.display = 'block';
-        socket.emit("get_quiz", [code, ((document.URL.split("?"))[1].split("="))[1].split("#")[0]]);
+        socket.emit("get_quiz", [code, ((document.URL.split("?"))[1].split("="))[1].split("#")[0], username]);
     }
 });
 socket.on('notlogedin', function (data) {
@@ -16,97 +18,113 @@ socket.on('notlogedin', function (data) {
         window.open("../../", "_self");
     }
 });
-let timer = 0;
 socket.on('get_quiz', function (data) {
     if (am_i(data[0], code)) {
-        if (data[1].timer !== 'None') {
-            timer = data[1].timer;
-        }
-        document.getElementById('loading').style.display = 'none';
-        quiz = data[1];
-        document.getElementById("top-image").style.backgroundImage = "url('" + quiz.topimage + "')";
-        document.getElementById("title").innerText = quiz.title;
-        document.getElementById("subtitle").innerText = quiz.subtitle;
-        document.getElementById("username").innerText = quiz.username;
-        document.getElementById("start").innerText = quiz.start;
-        document.getElementById("endimage").style.backgroundImage = quiz.end_image;
-        document.getElementById("youhave").innerText = quiz.end_youhave;
-        document.getElementById("points").innerText = quiz.end_thepoints;
-        let div = document.getElementById('questions');
-        for (let i in quiz.questions) {
-            let question = document.createElement("div");
-            question.setAttribute("class", `question`);
-            question.setAttribute("id", parseInt(i) + 1);
+        if (data[1] !== 'none' && data[1] !== 'Done') {
+            if (data[1].timer !== 'None') {
+                timer = data[1].timer;
+            }
+            document.getElementById('loading').style.display = 'none';
+            quiz = data[1];
+            document.getElementById("top-image").style.backgroundImage = "url('" + quiz.topimage + "')";
+            document.getElementById("title").innerText = quiz.title;
+            document.getElementById("subtitle").innerText = quiz.subtitle;
+            document.getElementById("username").innerText = quiz.username;
+            document.getElementById("start").innerText = quiz.start;
+            document.getElementById("endimage").style.backgroundImage = quiz.end_image;
+            document.getElementById("youhave").innerText = quiz.end_youhave;
+            document.getElementById("points").innerText = quiz.end_thepoints;
+            let div = document.getElementById('questions');
+            for (let i in quiz.questions) {
+                let question = document.createElement("div");
+                question.setAttribute("class", `question`);
+                question.setAttribute("id", i + 1);
 
-            let image = document.createElement('div');
-            image.style.backgroundImage = "url('" + quiz.questions[i].image + "')";
-            image.setAttribute('class', 'img');
+                let image = document.createElement('div');
+                image.style.backgroundImage = "url('" + quiz.questions[i].image + "')";
+                image.setAttribute('class', 'img');
 
-            let questionDiv = document.createElement('div');
-            questionDiv.setAttribute('class', 'questionDiv');
+                let questionDiv = document.createElement('div');
+                questionDiv.setAttribute('class', 'questionDiv');
 
-            if (quiz.questions[i].type.slice(0, 6) === 'answer') {
-                let questionText = document.createElement("h3");
-                questionText.innerText = quiz.questions[i].question;
-                questionDiv.appendChild(questionText);
-                let trueAnswerIndex = 0;
-                for (let answerIndex in quiz.questions[i].answer) {
-                    if (quiz.questions[i].answer[answerIndex].true) {
-                        trueAnswerIndex = answerIndex;
+                if (quiz.questions[i].type.slice(0, 6) === 'answer') {
+                    let questionText = document.createElement("h3");
+                    questionText.innerText = quiz.questions[i].question;
+                    questionDiv.appendChild(questionText);
+                    let trueAnswerIndex = 0;
+                    for (let answerIndex in quiz.questions[i].answer) {
+                        if (quiz.questions[i].answer[answerIndex].true) {
+                            trueAnswerIndex = answerIndex;
+                        }
+                    }
+                    for (let answerIndex in quiz.questions[i].answer) {
+                        let answerButton = document.createElement('button');
+                        answerButton.setAttribute('class', `answer${parseInt(i) + 1}`);
+                        answerButton.setAttribute('onclick', `check(${parseInt(i) + 1},${answerIndex},${trueAnswerIndex},${quiz.questions[i].answer[answerIndex].true})`);
+                        answerButton.innerText = quiz.questions[i].answer[answerIndex].text;
+                        questionDiv.appendChild(answerButton);
                     }
                 }
-                for (let answerIndex in quiz.questions[i].answer) {
-                    let answerButton = document.createElement('button');
-                    answerButton.setAttribute('class', `answer${parseInt(i) + 1}`);
-                    answerButton.setAttribute('onclick', `check(${parseInt(i) + 1},${answerIndex},${trueAnswerIndex},${quiz.questions[i].answer[answerIndex].true})`);
-                    answerButton.innerText = quiz.questions[i].answer[answerIndex].text;
-                    questionDiv.appendChild(answerButton);
+                if (quiz.questions[i].type === 'typing') {
+                    let questionText = document.createElement("h3");
+                    questionText.innerText = quiz.questions[i].question;
+                    questionDiv.appendChild(questionText);
+                    let answerInput = document.createElement("input");
+                    answerInput.setAttribute('class', `answer${parseInt(i) + 1}`);
+                    let checkAnswer = document.createElement("button");
+                    checkAnswer.innerText = quiz.check;
+                    checkAnswer.setAttribute('class', `check check${parseInt(i) + 1}`);
+                    checkAnswer.setAttribute('onclick', `checkInput(${parseInt(i) + 1},'${quiz.questions[i].answer}')`);
+                    questionDiv.appendChild(answerInput);
+                    questionDiv.appendChild(checkAnswer);
                 }
-            }
-            if (quiz.questions[i].type === 'typing') {
-                let questionText = document.createElement("h3");
-                questionText.innerText = quiz.questions[i].question;
-                questionDiv.appendChild(questionText);
-                let answerInput = document.createElement("input");
-                answerInput.setAttribute('class', `answer${parseInt(i) + 1}`);
-                let checkAnswer = document.createElement("button");
-                checkAnswer.innerText = quiz.check;
-                checkAnswer.setAttribute('class', `check check${parseInt(i) + 1}`);
-                checkAnswer.setAttribute('onclick', `checkInput(${parseInt(i) + 1},'${quiz.questions[i].answer}')`);
-                questionDiv.appendChild(answerInput);
-                questionDiv.appendChild(checkAnswer);
-            }
-            if (quiz.questions[i].type === 'text') {
-                let text = document.createElement("p");
-                text.setAttribute('class', `answer${parseInt(i) + 1}`);
-                text.innerText = quiz.questions[i].text;
-                questionDiv.appendChild(text);
-            }
+                if (quiz.questions[i].type === 'text') {
+                    let text = document.createElement("p");
+                    text.setAttribute('class', `answer${parseInt(i) + 1}`);
+                    text.innerText = quiz.questions[i].text;
+                    questionDiv.appendChild(text);
+                }
 
-            let nextButton = document.createElement('a');
-            nextButton.innerText = quiz.next;
-            if (quiz.questions[parseInt(i) + 1]) {
-                nextButton.setAttribute('href', `#${parseInt(i) + 2}`);
+                let nextButton = document.createElement('a');
+                nextButton.innerText = quiz.next;
+                if (quiz.questions[parseInt(i) + 1]) {
+                    nextButton.setAttribute('onclick', `scrollToNext()`);
+                } else {
+                    nextButton.innerText = quiz.seepoints;
+                    nextButton.setAttribute('href', `#end`);
+                }
+                questionDiv.appendChild(nextButton);
+
+                question.appendChild(image);
+                question.appendChild(questionDiv);
+                div.appendChild(question);
+            }
+            window.scrollTo(0, 0);
+            window.onscroll = function () {
+                if (document.documentElement.scrollTop > 20) {
+                    start();
+                    window.onscroll = function () {
+                    };
+                }
+            };
+        } else {
+            if (data[1] === 'none') {
+                window.open('../page-not-found', '_self');
             } else {
-                nextButton.innerText = quiz.seepoints;
-                nextButton.setAttribute('href', `#end`);
+                document.getElementById("top-image").style.backgroundImage = "url('../style/quiznotfound.png')";
+                document.getElementById("title").innerText = 'This Quiz is not available for you';
             }
-            questionDiv.appendChild(nextButton);
-
-            question.appendChild(image);
-            question.appendChild(questionDiv);
-            div.appendChild(question);
         }
     }
-    window.scrollTo(0, 0);
-    window.onscroll = function () {
-        if (document.documentElement.scrollTop > 20) {
-            start();
-            window.onscroll = function () {
-            };
-        }
-    };
 });
+
+function scrollToNext() {
+    window.scrollTo({
+        top: document.documentElement.scrollTop + 854,
+        left: 0,
+        behavior: 'smooth'
+    });
+}
 
 function start() {
     window.scrollTo({
@@ -137,29 +155,35 @@ function check(q, a, t, b) {
     document.getElementsByClassName("answer" + q)[t].style.background = "#29a329";
     document.getElementsByClassName("answer" + q)[t].style.borderColor = "#29a329";
     document.getElementsByClassName("answer" + q)[t].style.color = "#fff";
-    document.getElementById("thepoints").innerText = parseInt(document.getElementById("thepoints").innerText) + 1;
+    points++;
+    document.getElementById("thepoints").innerText = points;
     if (b === false) {
         document.getElementsByClassName("answer" + q)[a].style.background = "#cc1211";
         document.getElementsByClassName("answer" + q)[a].style.borderColor = "#cc1211";
         document.getElementsByClassName("answer" + q)[a].style.color = "#fff";
-        document.getElementById("thepoints").innerText = parseInt(document.getElementById("thepoints").innerText) - 1;
+        points--;
+        document.getElementById("thepoints").innerText = points;
     }
     for (var i = 0; i < document.getElementsByClassName("answer" + q).length; i++) {
         document.getElementsByClassName("answer" + q)[i].disabled = true;
     }
+    socket.emit('changeQuizResult', {quizId: quiz.id, username, points});
 }
 
 function checkInput(q, t) {
     document.getElementsByClassName("answer" + q)[0].style.background = "#29a329";
     document.getElementsByClassName("answer" + q)[0].style.borderColor = "#29a329";
     document.getElementsByClassName("answer" + q)[0].style.color = "#fff";
-    document.getElementById("thepoints").innerText = parseInt(document.getElementById("thepoints").innerText) + 1;
+    points++;
+    document.getElementById("thepoints").innerText = points;
     if (document.getElementsByClassName("answer" + q)[0].value !== t) {
         document.getElementsByClassName("answer" + q)[0].style.background = "#cc1211";
         document.getElementsByClassName("answer" + q)[0].style.borderColor = "#cc1211";
         document.getElementsByClassName("answer" + q)[0].style.color = "#fff";
-        document.getElementById("thepoints").innerText = parseInt(document.getElementById("thepoints").innerText) - 1;
+        points--;
+        document.getElementById("thepoints").innerText = points;
     }
     document.getElementsByClassName("answer" + q)[0].disabled = true;
     document.getElementsByClassName("check" + q)[0].disabled = true;
+    socket.emit('changeQuizResult', {quizId: quiz.id, username, points});
 }
--- server.js
+++ server.js
@@ -83,23 +83,23 @@ server.listen(3000);
 
 function saveDataOnJson() {
     fs.writeJson('./json/events.json', {"data": events}, err => {
-        if (err) return console.error(err);
+        if (err) return console.log(err);
     });
     fs.writeJson('./json/quiz.json', {"data": quiz}, err => {
-        if (err) return console.error(err);
+        if (err) return console.log(err);
     });
     fs.writeJson('./json/users.json', {"users": users}, err => {
-        if (err) return console.error(err);
+        if (err) return console.log(err);
     });
     fs.writeJson('./json/logInData.json', {"data": logInData}, err => {
-        if (err) return console.error(err);
+        if (err) return console.log(err);
     });
     console.log("All Info saved in json files");
 }
 
 function updateEventsStatus() {
     for (let i in events) {
-        if (events[i].type == "published") {
+        if (events[i].type === "published") {
             if (events[i].end < date.getUTCFullYear() + "-" + ("0" + (date.getMonth() + 1)).slice(-2) + "-" + ("0" + date.getUTCDate()).slice(-2)) {
                 events[i].type = "ended";
             }
@@ -120,7 +120,7 @@ io.on('connection', function (socket) {
         if (info && info != "null") {
             let username = false;
             for (var i in users) {
-                if (info.username == users[i].username && info.password == users[i].password) {
+                if (info.username === users[i].username && info.password === users[i].password) {
                     username = true;
                 }
             }
@@ -150,7 +150,7 @@ io.on('connection', function (socket) {
     socket.on("get_events", function (data) {
         var published_events = [];
         for (var i in events) {
-            if (events[i].type == "published") {
+            if (events[i].type === "published") {
                 published_events.push(events[i]);
             }
         }
@@ -159,7 +159,7 @@ io.on('connection', function (socket) {
     socket.on("joinEvent", function (data) {
         var code = data[0];
         for (var i in events) {
-            if (events[i].id == data[2]) {
+            if (events[i].id === data[2]) {
                 events[i].users.push(data[1]);
                 io.sockets.emit("joinEvent", code);
             }
@@ -170,7 +170,7 @@ io.on('connection', function (socket) {
         var code = data[1];
         var username = true;
         for (var i in users) {
-            if (info.username == users[i].username) {
+            if (info.username === users[i].username) {
                 io.sockets.emit("no_username", code);
                 username = false;
             }
@@ -185,8 +185,8 @@ io.on('connection', function (socket) {
         var code = data[1];
         var username = false;
         for (var i in users) {
-            if (info.username == users[i].username) {
-                username = info.password == users[i].password;
+            if (info.username === users[i].username) {
+                username = info.password === users[i].password;
             }
         }
         if (username) {
@@ -217,14 +217,14 @@ io.on('connection', function (socket) {
     socket.on("get_event", function (data) {
         var the_event = "none";
         for (var i in events) {
-            if (events[i].link == data[1]) {
+            if (events[i].link === data[1]) {
                 the_event = events[i];
                 break;
             }
         }
         for (var i in the_event.quizzes) {
             for (var j in quiz) {
-                if (the_event.quizzes[i].link == quiz[j].id) {
+                if (the_event.quizzes[i].link === quiz[j].id) {
                     var the_date = the_event.quizzes[i].date;
                     the_event.quizzes[i] = quiz[j];
                     the_event.quizzes[i].date = the_date
@@ -236,17 +236,50 @@ io.on('connection', function (socket) {
     socket.on("get_quiz", function (data) {
         var the_quiz = "none";
         for (var i in quiz) {
-            if (quiz[i].id == data[1]) {
-                the_quiz = quiz[i];
+            if (quiz[i].id === data[1]) {
+                let hasPoints = false;
+                for (let resultI in quiz[i].results) {
+                    if (quiz[i].results[resultI].username === data[2]) {
+                        hasPoints = true;
+                        the_quiz = "Done";
+                        break;
+                    }
+                }
+                if (!hasPoints) {
+                    the_quiz = quiz[i];
+                }
                 break;
             }
         }
         io.sockets.emit("get_quiz", [data[0], the_quiz]);
-    })
+    });
+    socket.on("changeQuizResult", function (data) {
+        for (let i in quiz) {
+            if (quiz[i].id === data.quizId) {
+                if (quiz[i].results) {
+                    let hasPoints = false;
+                    for (let resultI in quiz[i].results) {
+                        if (quiz[i].results[resultI].username === data.username) {
+                            quiz[i].results[resultI] = {username: data.username, points: data.points};
+                            hasPoints = true;
+                            break;
+                        }
+                    }
+                    if (!hasPoints) {
+                        quiz[i].results.push({username: data.username, points: data.points});
+                    }
+                } else {
+                    quiz[i].results = [];
+                    quiz[i].results.push({username: data.username, points: data.points});
+                }
+                break;
+            }
+        }
+    });
     socket.on("get_the_quizzes", function (data) {
         var the_quizzes = [];
         for (var i in quiz) {
-            if (quiz[i].username == data[1] && quiz[i].type == "published") {
+            if (quiz[i].username === data[1] && quiz[i].type === "published") {
                 the_quizzes.push(quiz[i]);
             }
         }
@@ -255,7 +288,7 @@ io.on('connection', function (socket) {
     socket.on("get_the_events", function (data) {
         var the_events = [];
         for (var i in events) {
-            if (events[i].username == data[1] && events[i].type == "published") {
+            if (events[i].username === data[1] && events[i].type === "published") {
                 the_events.push(events[i]);
             }
         }
@@ -264,13 +297,13 @@ io.on('connection', function (socket) {
     socket.on("get_my_works", function (data) {
         var the_quizzes = [];
         for (var i in quiz) {
-            if (quiz[i].username == data[1] && quiz[i].type != 'deleted') {
+            if (quiz[i].username === data[1] && quiz[i].type != 'deleted') {
                 the_quizzes.push(quiz[i]);
             }
         }
         var the_events = [];
         for (var i in events) {
-            if (events[i].username == data[1] && events[i].type != 'deleted') {
+            if (events[i].username === data[1] && events[i].type != 'deleted') {
                 the_events.push(events[i]);
             }
         }
@@ -283,7 +316,7 @@ io.on('connection', function (socket) {
         }
         var newquiz = true;
         for (var i in quiz) {
-            if (quiz[i].id == data.id) {
+            if (quiz[i].id === data.id) {
                 quiz[i] = data;
                 newquiz = false;
             }
@@ -291,7 +324,7 @@ io.on('connection', function (socket) {
         if (newquiz) {
             quiz.push(data);
         }
-        if (data.type == 'deleted') {
+        if (data.type === 'deleted') {
             io.sockets.emit('quiz_deleted', code);
         }
     })
@@ -302,7 +335,7 @@ io.on('connection', function (socket) {
         }
         var newevent = true;
         for (var i in events) {
-            if (events[i].id == data.id) {
+            if (events[i].id === data.id) {
                 events[i] = data;
                 newevent = false;
             }
@@ -310,14 +343,14 @@ io.on('connection', function (socket) {
         if (newevent) {
             events.push(data);
         }
-        if (data.type == 'deleted') {
+        if (data.type === 'deleted') {
             io.sockets.emit('event_deleted', code);
         }
     })
     socket.on("get_the_profile", function (data) {
         let profile = false;
         for (var i in users) {
-            if (users[i].username == data[1]) {
+            if (users[i].username === data[1]) {
                 var user_info = {};
                 for (var j in users[i]) {
                     if (j != "password") {
